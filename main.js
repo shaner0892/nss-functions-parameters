a0a769cfae202eeb6ccf3c4d52da1adb1af4027c
Hilary Shaner <hilaryshaner@gmail.com>
2022-01-12 15:47:10 -0600

    added function and invoked

---
+++ main.js
@@ -0,0 +1,11 @@
+const quarterValue = (originalNumber) => {
+    const quarter = originalNumber / 4
+
+    return quarter
+}
+
+// Invoke the function and store the return value
+const newNumber = quarterValue(12)
+
+// Log the return value
+console.log(newNumber)
